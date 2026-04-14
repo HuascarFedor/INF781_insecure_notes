0fc01ceb967234d9e65d631d09c1eea3c582b9a4
HuascarFedor <huascar.fedor@gmail.com>
2026-04-14 18:01:50 -0400

    atacante agregado

--- notas/index.php
+++ notas/index.php
@@ -1,1 +1,2 @@
-aqui index notas
+// lista de las notas del usuario con enlaces a crear nota, modificar una nota
+// eliminar una nota
---
+++ perfil.php
@@ -0,0 +1,5 @@
+// Informacion del usuario
+
+// Informacion sobre las notas que ha creado
+
+// Enlace para cerrar sesión
